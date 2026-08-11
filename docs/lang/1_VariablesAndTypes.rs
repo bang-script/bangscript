b4b0a447f2aad7d87e3488f2fc1d9e497891782c
stefpga <stefandragan95@gmail.com>
2026-08-11 16:10:13 +0300

    Revise comments on variables and types in Rust
    Updated comments and examples for variable declaration and types.

---
+++ docs/lang/1_VariablesAndTypes.rs
@@ -0,0 +1,27 @@
+// Variables declaration
+//    Variables can be defined with either:
+//       - let (mutable)
+//       - const (immutable)
+
+/*
+   Example
+*/
+
+let name = "BangScript"
+const _pi = 3.14159265
+
+// Types
+/*
+      Variables can be explicitly typed, or inferred by the compiler
+        Examples:
+            - String
+            - Float
+            - Integer
+            - List<T>
+*/
+
+//      Example:
+
+let age :: Integer = 2763
+let id :: Float = 2.468
+let age = "2763"
